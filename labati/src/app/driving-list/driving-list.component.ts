21614665a1b7ef900f74a4794be292a26e7c984b
NicholasLabati <labati.nicholas.luigi@iisgalvanimi.edu.it>
2020-03-25 17:53:59 +0000

    Punto 2

---
+++ labati/src/app/driving-list/driving-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Drive } from '../drive.model';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-driving-list',
+  templateUrl: './driving-list.component.html',
+  styleUrls: ['./driving-list.component.css']
+})
+export class DrivingListComponent implements OnInit {
+  driveList : Drive[];
+  obs_drive : Observable<Drive[]>;
+  constructor(public http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.obs_drive = this.http.get<Drive[]>('https://my-json-server.typicode.com/malizia-g/fine_anno_exp/mezzi')
+    this.obs_drive.subscribe(this.get_driving)
+  }
+
+  get_driving = (auto: Drive[]) => {
+    this.driveList = auto;
+  }
+}
